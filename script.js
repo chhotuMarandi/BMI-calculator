03514f65d38954ad914ac6297cb978b34b7b37fb
chhotu marandi <cmarandi29@gmail.com>
2023-09-19 02:17:00 +0530

    add some conditions

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 
-function calculateBMI(){
+
+function calculateBMI() {
   let height = document.getElementById('height-input').value;
   let weight = document.getElementById('weight-input').value;
 
-  (document.getElementById('bmi').innerHTML =
-    (weight / height / height) * 10000);
+  let bmiResult = document.getElementById('bmi');
+
+  let bmi = (weight / height / height) * 10000;
+  const bmiValue = bmi.toFixed(2);
+
+  if (bmiValue < 18.5) {
+    bmiResult.innerHTML = `underweight (${bmiValue})`;
+  } else if (bmiValue > 18.5 && bmi < 24.9) {
+    bmiResult.innerHTML = `Normal (${bmiValue})`;
+  } else if (bmiValue > 25 && bmi < 29.9) {
+    bmiResult.innerHTML = `Overweight (${bmiValue})`;
+  } else {
+    bmiResult.innerHTML = `Obese (${bmiValue})`;
+  }
+
+  let clear = document.getElementById('clear');
+
+  clear.addEventListener('click', () => {
+    console.log('hi');
+  });
 }
 
-calculateBMI();
-
-
-
-
+// calculateBMI();
 
 /* let age = document.getElementById("age");
 let height = document.getElementById("height-input").value;
